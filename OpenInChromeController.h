2df6ef597114bbd6423bc9cccf49ab8bc658ac1f
jakescott <jake.net@gmail.com>
2013-03-06 20:34:31 +1300

    The easiest way to have your iOS app open links in Chrome is to use the OpenInChromeController class

---
+++ OpenInChromeController.h
@@ -0,0 +1,13 @@
+//
+//  OpenInChromeController.h
+//  Navbar
+//
+//  Created by Jake Scott on 6/03/13.
+//  Copyright (c) 2013 superlogical. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenInChromeController : NSObject
+
+@end
---
+++ OpenInChromeController.m
@@ -0,0 +1,13 @@
+//
+//  OpenInChromeController.m
+//  Navbar
+//
+//  Created by Jake Scott on 6/03/13.
+//  Copyright (c) 2013 superlogical. All rights reserved.
+//
+
+#import "OpenInChromeController.h"
+
+@implementation OpenInChromeController
+
+@end
